7b9caeedb40b0156d68377d137424e388b64de6c
Trung Tran <trung.tran@ettus.com>
2018-02-19 06:38:48 -0800

    WIP

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path')
+var app = express();
+port = 8000;
+function log(req, err, next){
+    console.log(new Date(), req.method, req.url)
+    next()
+}
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(log)
+app.get('/',function(req,res){
+    //res.write('Hello World')
+    res.sendFile(path.join(__dirname + '/sample_file.log'))
+    console.log(path.join(__dirname + '/simplegraph.html'))
+})
+app.listen(port,function(err,resp){
+    if (err){
+        console.log(`Server errror`)
+    }else{
+        console.log('Server started on port '+port)
+    }
+});
